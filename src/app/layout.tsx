d38cfcb314d35941c23b931b81b54890cc8ca95f
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-05-28 19:25:37 +0300

    Eka commit.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Lora } from "next/font/google";
 import "./globals.css";
 
+import { HeaderComponent } from "@/components/header-component";
+
 const inter = Inter({ subsets: ["latin"] });
+const lora = Lora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header
+          className={`${lora.className} flex justify-center p-4 bg-primary text-white h-[100px]`}
+        >
+          <HeaderComponent />
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/header-component.tsx
@@ -0,0 +1,7 @@
+export const HeaderComponent: React.FC = () => {
+  return (
+    <>
+      <h1 className="text-5xl text-yellow-500 font-bold">Pennywise</h1>
+    </>
+  );
+};
